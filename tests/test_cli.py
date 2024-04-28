ae25e8c55ed19c97b1bc4043a3af8e7164ee5b76
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-04-28 12:02:08 +0300

    add cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,3 +7,11 @@ def test_parse_args():
     args = parse_args()
     assert args.first_file == 'file1.json'
     assert args.second_file == 'file2.json'
+    assert args.format == 'plain'
+
+
+def test_parse_args_help():
+    conslole_output = open('tests/fixtures/help_output.txt', 'r').read()
+    sys.argv = ['gendiff', '-h']
+    args = parse_args()
+    assert args == conslole_output
