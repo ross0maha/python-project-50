299592e8ae72dcb8f35b2d5e512743a60a209911
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-05-07 23:21:57 +0300

    add tests for cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import sys
+import pytest
 from gendiff.cli import parse_args
 
 
@@ -10,8 +11,21 @@ def test_parse_args():
     assert args.format == 'plain'
 
 
-# def test_parse_args_help():
-#     conslole_output = open('tests/fixtures/help_output.txt', 'r').read()
-#     sys.argv = ['gendiff', '-h']
-#     args = parse_args()
-#     assert args == conslole_output
+def test_parse_args_help(capsys):
+    with open('tests/fixtures/help_output.txt', 'r') as f:
+        help_text = f.read()
+    sys.argv = ['gendiff', '-h']
+    with pytest.raises(SystemExit):
+        parse_args()
+    out, err = capsys.readouterr()
+    assert out == help_text
+
+
+def test_parse_args_empty(capsys):
+    with open('tests/fixtures/cli_out.txt', 'r') as f:
+        cli_out = f.read()
+    sys.argv = ['gendiff']
+    with pytest.raises(SystemExit):
+        parse_args()
+    out, err = capsys.readouterr()
+    assert err == cli_out
