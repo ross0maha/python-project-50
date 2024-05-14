548ef218e867c9a449cdca9fea21cc9d991a5434
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-05-14 21:33:53 +0300

    fix linter error

--- gendiff/formatters/stylish_format.py
+++ gendiff/formatters/stylish_format.py
@@ -6,8 +6,8 @@ def stringify_val(data, depth):
         return data
     tmp = ["{"]
     for k, v in data.items():
-        tmp.append(f"\n{'  '*depth}  {k}: {stringify_val(v, depth+2)}")
-    tmp.append(f"\n{'  '*(depth-1)}}}")
+        tmp.append(f"\n{'  '*depth}  {k}: {stringify_val(v, depth + 2)}")
+    tmp.append(f"\n{'  '*(depth - 1)}}}")
     return ''.join(tmp)
 
 
@@ -23,17 +23,17 @@ def stringify_diff(diff, depth=1):
         status = v['status']
 
         if status == 'nested':
-            lst.append(f"{'  '*depth}  {k}: {{\n")
-            lst.append(f"{stringify_diff(v['value'], depth+2)}")
-            lst.append(f"{'  '*(depth+1)}}}\n")
+            lst.append(f"{'  ' * depth}  {k}: {{\n")
+            lst.append(f"{stringify_diff(v['value'], depth + 2)}")
+            lst.append(f"{'  ' * (depth + 1)}}}\n")
         elif status == 'changed':
-            lst.append(f"{'  '*depth}- {k}: "
-                       f"{stringify_val(v['old_value'], depth+2)}\n")
+            lst.append(f"{'  ' * depth}- {k}: "
+                       f"{stringify_val(v['old_value'], depth + 2)}\n")
             lst.append(f"{'  ' * depth}+ {k}: "
-                       f"{stringify_val(v['new_value'], depth+2)}\n")
+                       f"{stringify_val(v['new_value'], depth + 2)}\n")
         else:
             lst.append(f"{'  ' * depth}{STATUS[status]}{k}: "
-                       f"{stringify_val(v['value'], depth+2)}\n")
+                       f"{stringify_val(v['value'], depth + 2)}\n")
     res = ''.join(lst)
     return res
 
