576529217294ff17beff33d85e133666badaa7e8
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-05-14 21:14:22 +0300

    cli args fix

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,7 +6,7 @@ def parse_args():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-F', '--format',
+    parser.add_argument('-f', '--format',
                         help='set format of output',
                         default='stylish', type=str)
     return parser.parse_args()
