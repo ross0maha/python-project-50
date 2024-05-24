6a4204e9479ee80d1ee978b332c5cd8b7481405c
Dmitry Yuroff <yuroff.dmitry@gmail.com>
2024-05-25 02:13:17 +0300

    minor update for test-coverage

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,6 +9,7 @@ json_4 = "tests/fixtures/file4.json"
 yaml_1 = "tests/fixtures/file1.yaml"
 yaml_2 = "tests/fixtures/file2.yaml"
 
+
 res_stylish_1 = "tests/fixtures/res_stylish_1.txt"
 res_stylish_2 = "tests/fixtures/res_stylish_2.txt"
 res_plain_1 = "tests/fixtures/res_plain_1.txt"
@@ -26,6 +27,9 @@ res_json_2 = "tests/fixtures/res_json_2.txt"
         (json_3, json_4, "stylish", res_stylish_2),
         (json_3, json_4, "plain", res_plain_2),
         (json_3, json_4, "json", res_json_2),
+        (yaml_1, yaml_2, "stylish", res_stylish_1),
+        (yaml_1, yaml_2, "plain", res_plain_1),
+        (yaml_1, yaml_2, "json", res_json_1),
     ],
 )
 def test_generate_diff(first_file, second_file, format_name, expected_file):
